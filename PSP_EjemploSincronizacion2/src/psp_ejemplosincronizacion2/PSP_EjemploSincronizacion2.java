d02255380275f40c515a2fe311b514e095bafeca
finfanterodal <finfanterodal@danielcastelao.org>
2019-11-15 09:09:06 +0100

    Init

---
+++ PSP_EjemploSincronizacion2/src/psp_ejemplosincronizacion2/PSP_EjemploSincronizacion2.java
@@ -0,0 +1,20 @@
+package psp_ejemplosincronizacion2;
+import java.io.File;
+
+/**
+ *
+ * @author dam2
+ */
+public class PSP_EjemploSincronizacion2 {
+    
+    File fichero= new File("textoLibro.txt");
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
